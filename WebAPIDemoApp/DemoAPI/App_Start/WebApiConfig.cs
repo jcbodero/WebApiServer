fdbc4edfa802370823da41e7d5537c526cb55915
jcbodero <j.boderoc@gmail.com>
2019-03-21 16:39:29 -0500

    Add files via upload

---
+++ WebAPIDemoApp/DemoAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DemoAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
